841621314786c07ebd0a81506a6b52a95463fdb9
Paul Moff <pavel@yakimoff.ru>
2015-06-02 12:55:25 +0400

    now user can upload avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :comment => "48x48#", :navbar => "32x32#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  attr_accessor :remove_avatar
+
+  before_save :delete_avatar, if: ->{ remove_avatar == '1' && !avatar_updated_at_changed? }
+
+  private
+
+  def delete_avatar
+    self.avatar = nil
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531145844) do
+ActiveRecord::Schema.define(version: 20150602075243) do
 
   create_table "commontator_comments", force: true do |t|
     t.string   "creator_type"
@@ -82,6 +82,10 @@ ActiveRecord::Schema.define(version: 20150531145844) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
